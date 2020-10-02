e03a4589afceceb3237a8254835f3430fd4adbe3
my name <you@exampl.com>
2020-10-02 12:56:48 +0600

    get appointment page connected to server and backend

--- src/Components/Appointment/Appointment.js
+++ src/Components/Appointment/Appointment.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import Navigation from '../Header/Navigation'
@@ -10,9 +10,37 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 const Appointment = () => {
     const [value, onChange] = useState(new Date());
     const [popupStyle, setPopupStyle] = useState("none")
-    const callPopup = (style) => {
+
+    const [treatmentTypes, setTreatmentTypes] = useState([])
+    const [bookings, setBookings] = useState({})
+
+    const callPopup = (style, treatment) => {
         setPopupStyle(style)
+        setBookings({ treatmentName: treatment.name, treatmentTime: treatment.time })
     }
+    const formValueHandler = e => {
+        const name = e.target.name
+        const value = e.target.value
+        if (name === 'Name') setBookings({ ...bookings, patientName: value })
+        else if (name === 'Number') setBookings({ ...bookings, patientPhone: value })
+        else if (name === 'Email') setBookings({ ...bookings, patientEmail: value })
+        else if (name === 'Date') setBookings({ ...bookings, appointmentDate: value })
+    }
+    const submitForm = e => {
+        e.preventDefault()
+        fetch('http://localhost:4000/appointmentBookings', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(bookings)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data.insertedCount))
+    }
+    useEffect(() => {
+        fetch(`http://localhost:4000/treatmentTypes`)
+            .then(res => res.json())
+            .then(data => setTreatmentTypes(data))
+    }, [])
 
     return (
         <>
@@ -34,68 +62,35 @@ const Appointment = () => {
             <section className="appointment-list mt-5">
                 <h4 className="text-center mb-3">Available Appointments on {value.toLocaleDateString()}</h4>
                 <div className="row all-appointment mx-auto">
-                    <div className="col-md-4 p-0 my-3">
-                        <div className="text-center mx-3 appointment-info">
-                            <h5>Teeth Orthodontics</h5>
-                            <h6>8:00 AM - 9:00 AM</h6>
-                            <p>10 Spaces Available</p>
-                            <button className="" onClick={() => callPopup("flex")}>Book Appointment</button>
-                        </div>
-                    </div>
-                    <div className="col-md-4 p-0 my-3">
-                        <div className="text-center mx-3 appointment-info">
-                            <h5>Teeth Orthodontics</h5>
-                            <h6>8:00 AM - 9:00 AM</h6>
-                            <p>10 Spaces Available</p>
-                            <button className="" onClick={() => callPopup("flex")}>Book Appointment</button>
-                        </div>
-                    </div>
-                    <div className="col-md-4 p-0 my-3">
-                        <div className="text-center mx-3 appointment-info">
-                            <h5>Teeth Orthodontics</h5>
-                            <h6>8:00 AM - 9:00 AM</h6>
-                            <p>10 Spaces Available</p>
-                            <button className="" onClick={() => callPopup("flex")}>Book Appointment</button>
-                        </div>
-                    </div>
-                    <div className="col-md-4 p-0 my-3">
-                        <div className="text-center mx-3 appointment-info">
-                            <h5>Teeth Orthodontics</h5>
-                            <h6>8:00 AM - 9:00 AM</h6>
-                            <p>10 Spaces Available</p>
-                            <button className="" onClick={() => callPopup("flex")}>Book Appointment</button>
-                        </div>
-                    </div>
-                    <div className="col-md-4 p-0 my-3">
-                        <div className="text-center mx-3 appointment-info">
-                            <h5>Teeth Orthodontics</h5>
-                            <h6>8:00 AM - 9:00 AM</h6>
-                            <p>10 Spaces Available</p>
-                            <button className="" onClick={() => callPopup("flex")}>Book Appointment</button>
-                        </div>
-                    </div>
-                    <div className="col-md-4 p-0 my-3">
-                        <div className="text-center mx-3 appointment-info">
-                            <h5>Teeth Orthodontics</h5>
-                            <h6>8:00 AM - 9:00 AM</h6>
-                            <p>10 Spaces Available</p>
-                            <button className="" onClick={() => callPopup("flex")}>Book Appointment</button>
-                        </div>
-                    </div>
+                    {
+                        treatmentTypes.map(treatments => {
+                            const { _id, name, space, time } = treatments
+                            return (
+                                <div key={_id} className="col-md-4 p-0 my-3">
+                                    <div className="text-center mx-3 appointment-info">
+                                        <h5>{name}</h5>
+                                        <h6>{time}</h6>
+                                        <p>{space} Spaces Available</p>
+                                        <button className="" onClick={() => callPopup("flex", treatments)}>Book Appointment</button>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
                 <br /><br />
             </section>
             <section style={{ "display": popupStyle }} className="appointment-popup">
                 <div className="form-holder mx-auto">
-                    <span className="quit" onClick={() => callPopup("none")}><FontAwesomeIcon icon={faTimes} /></span>
-                    <h5 className="text-center subject">Teeth Orthodontics</h5>
+                    <span className="quit" onClick={() => callPopup("none", {})}><FontAwesomeIcon icon={faTimes} /></span>
+                    <h5 className="text-center subject">{bookings.treatmentName}</h5>
                     <form className="d-flex flex-column submit-appointment mx-auto">
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="text" />
-                        <input type="submit" className="ml-auto set-appointment text-uppercase" value="Send" />
+                        <input type="text" value={` On ${value.toLocaleDateString()} at ${bookings.treatmentTime}`} readOnly />
+                        <input name="Name" type="text" placeholder="Your Name" onBlur={formValueHandler} />
+                        <input name="Number" type="number" placeholder="Your Phone Number" onBlur={formValueHandler} />
+                        <input name="Email" type="email" placeholder="Your Email" onBlur={formValueHandler} />
+                        <input name="Date" type="date" onBlur={formValueHandler} />
+                        <input type="submit" className="ml-auto set-appointment text-uppercase" value="Send" onClick={submitForm} />
                     </form>
                 </div>
             </section>
